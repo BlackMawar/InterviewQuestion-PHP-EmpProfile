80db13e3631d22362af8231b9c999dd4a3ea5b69
AnifRastari <anif.rastari@gmail.com>
2025-05-28 16:16:23 +0800

    setting up laravel

---
+++ Employees-Form/resources/views/form.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Welcome to the first page you ever create using laravel. Let's try this.
+</body>
+</html>
